e97e12826c6beb810bf9a446466d14ea327af747
Roberta Nascimento <roberta.nask@gmail.com>
2025-05-27 12:31:18 -0300

    Adições e testes
    Adicionaram-se atributos ao objeto Carta e testes foram realizados para análise de seu correto funcionamento.

--- mtg/src/Main.java
+++ mtg/src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Modelos.Carta;
+
 public class Main {
     public static void main(String[] args) {
+        Carta cartaTeste = new Carta();
 
+        cartaTeste.setNome("Reformadora da Metrópole");
+        cartaTeste.setCor("Branca");
+        cartaTeste.setCusto("2W");
+        cartaTeste.setCmc(3);
+        cartaTeste.setTipo("Criatura");
+        cartaTeste.setSubtipo("Anjo Clérigo");
+        cartaTeste.setDescricao("""
+            Voar, vigilância
+            Você tem resistência a magia.
+            Toda vez que Reformadora da Metrópoloe sofre dano, você ganha aquela quantidade de pontos de vida.""");
+        cartaTeste.setPoder("2");
+        cartaTeste.setResistencia("3");
+        cartaTeste.setRaridade("Rara");
+        cartaTeste.setEdicao("Marcha das Máquinas: Consequências (2023)");
+        cartaTeste.setFoil(false);
+
+        cartaTeste.apresentaCarta();
     }
 }
--- mtg/src/Modelos/Carta.java
+++ mtg/src/Modelos/Carta.java
@@ -1,18 +1,78 @@
 package Modelos;
 
 public class Carta {
-    String nome;
-    String custo;
-    String tipo;
-    String subtipo;
-    String descricao;
-    String ataque;
-    String resistencia;
+    private String nome;
+    private String cor;
+    private String custo;
+    private int cmc;
+    private String tipo;
+    private String subtipo;
+    private String descricao;
+    private String poder;
+    private String resistencia;
+    private String raridade;
+    private String edicao;
+    private boolean foil;
 
-    void apresentaCarta() {
+    public void setNome(String nome) {
+        this.nome = nome;
+    };
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setCusto(String custo) {
+        this.custo = custo;
+    }
+
+    public void setCmc(int cmc) {
+        this.cmc = cmc;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setSubtipo(String subtipo) {
+        this.subtipo = subtipo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPoder(String poder) {
+        this.poder = poder;
+    }
+
+    public void setResistencia(String resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public void setRaridade(String raridade) {
+        this.raridade = raridade;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public void setFoil(boolean foil) {
+        this.foil = foil;
+    }
+
+    public void apresentaCarta() {
         System.out.println("Carta: " + nome);
+        System.out.println("Cor: " + cor);
         System.out.println("Custo: " + custo);
+        System.out.println("CMC: " + cmc);
         System.out.println("Tipo: " + tipo);
         System.out.println("Subtipo: " + subtipo);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Ataque/Resistência: " + poder + "/" + resistencia);
+        System.out.println("Raridade: " + raridade);
+        System.out.println("Edição: " + edicao);
+        System.out.println("Foil: " + foil);
     }
 }
