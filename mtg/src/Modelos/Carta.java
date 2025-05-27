a5d52b07a77e7f8dd85680b4f7bfd3cc33d0cae5
Roberta Nascimento <roberta.nask@gmail.com>
2025-05-27 09:50:57 -0300

    Primeiro commit de teste
    Testando

---
+++ mtg/src/Modelos/Carta.java
@@ -0,0 +1,13 @@
+package Modelos;
+
+public class Carta {
+    String nome;
+    String custo;
+    String tipo;
+    String subtipo;
+    String descricao;
+    String edicao;
+    String raridade;
+    String ataque;
+    String resistencia;
+}
